97052d00dfe626c271b5cd931c046962a94d0383
mackay <johnmackay150@gmail.com>
2019-06-10 20:20:44 +0900

    update compnay text

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 document.getElementById('but').onclick = function (){
-	document.getElementById('mac').innerHTML='<h1>HOGE HOGE</h1>';
+	document.getElementById('mac').innerHTML='<h1>Acompany</h1>';
 }
